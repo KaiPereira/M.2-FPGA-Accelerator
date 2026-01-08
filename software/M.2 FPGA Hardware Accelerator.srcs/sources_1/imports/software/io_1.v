2aadc55c017b95cce2d12d0e083f993b0c572c17
Kai Pereira <kaipereira2020@gmail.com>
2026-01-07 20:43:05 -0800

    switching to RTL vivado

---
+++ software/M.2 FPGA Hardware Accelerator.srcs/sources_1/imports/software/io_1.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2025.2 (lin64) Build 6299465 Fri Nov 14 12:34:56 MST 2025
+// Date        : Wed Jan  7 20:41:35 2026
+// Host        : iphone running 64-bit Ubuntu 24.04.3 LTS
+// Command     : write_verilog -mode pin_planning -force -port_diff_buffers
+//               /home/kai/projects/pcbs/working/FPGA-Accelerator/software/io_1.v
+// Design      : ios
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a50tfgg484-2
+// --------------------------------------------------------------------------------
+module ios();
+
+  // internal wires associated with differential buffers
+
+
+  // differential buffers
+
+
+endmodule
